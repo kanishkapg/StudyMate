5143bdb85b6fc8101802cb6dec9c9f46c307af2a
Kanishka Praveen <kanishkapg1121@gmail.com>
2024-08-11 07:27:38 +0530

    Header changed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { BiTimer } from 'react-icons/bi';
 import logo from '../../assets/images/HomePageIcons/logo.png';
+import AlarmIcon from '../../assets/images/HomePageIcons/timer.gif'
 
 const NavLinks = [
   {
@@ -19,7 +20,7 @@ const NavLinks = [
 
 const Header = () => {
   return (
-    <header className="header flex items-center py-[40px]">
+    <header className="header flex items-center py-[10px]">
       <div className="container h-[100px] flex items-center justify-between">
         {/* Logo */}
         <div className="w-[192px] mt-2 ">
@@ -29,10 +30,11 @@ const Header = () => {
         {/* Center Button */}
         <div className="relative">
           <button
-            className="flex items-center gap-0 bg-gradient-to-b from-[#649EF3] to-[#0D63E3] shadow-md w-[316px] h-[48px] rounded-lg text-white radius-[15px]"
+            className="flex items-center justify-center gap-0 bg-gradient-to-b from-[#649EF3] to-[#0D63E3] 
+            shadow-md w-[316px] h-[48px] rounded-lg text-white radius-[15px]"
             style={{ top: '35px', left: '233px' }}
           >
-            <BiTimer className="w-[28px] h-[27px] ml-3" />
+            <img src={AlarmIcon} alt="" className='w-[20px]'/>
             <span className="ml-4 font-extrabold text-[24px] leading-[29.05px]">
               Set Your Study Plan
             </span>
@@ -41,7 +43,7 @@ const Header = () => {
 
         {/* Navigation Links */}
         <div className="flex items-center">
-          <ul className="flex items-center space-x-[2.7rem]">
+          <ul className="flex items-center space-x-[88px] w-auto">
             {NavLinks.map((link, index) => (
               <li key={index}>
                 <NavLink
